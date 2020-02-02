e5ec0a3a0875ddbb55c463bd9f028af7f52c240d
Yaroslav Nestor <nestorslavko45@gmail.com>
2020-02-02 23:23:23 +0200

    Finish filters for genres.

--- frontend/src/components/main/Filters.js
+++ frontend/src/components/main/Filters.js
@@ -1,10 +1,13 @@
 import React from "react";
 import './../styles/Filters.css'
+import Genres from "../forms/GenresOptions";
 
 function Filters() {
     return (
-        <div className="col-sm-2 filters">
+        <div className="col-sm-3 filters">
             <h6 className="filterTitle">Filters</h6>
+            <span className="filtersName">Select genres</span>
+            <Genres/>
         </div>
     )
 }
