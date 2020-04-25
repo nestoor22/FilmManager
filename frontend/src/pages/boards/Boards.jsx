a97e8b96750cff3158e8515792c6ed90bb993802
Yaroslav Nestor <nestorslavko45@gmail.com>
2020-04-26 00:01:35 +0300

    Create boards grid

--- frontend/src/pages/boards/Boards.jsx
+++ frontend/src/pages/boards/Boards.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-import useStyles from "./styles";
-import BoardsStatisticsTab from "../../components/boards-statistics-tabs/BoardsStatisticsTab";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import Typography from "@material-ui/core/Typography";
 import AppHeader from "../../components/app-header/AppHeader";
+import BoardsStatisticsTab from "../../components/boards-statistics-tabs/BoardsStatisticsTab";
+import useStyles from "./styles";
+import Button from "@material-ui/core/Button";
 
 const testChartData = [
   { name: "Private", value: 43, legendTitle: "Private" },
@@ -18,17 +21,114 @@ const Boards = () => {
   return (
     <div className={classes.root}>
       <AppHeader />
-      <div className={classes.verticalTabs}>
-        <BoardsStatisticsTab
-          title="Your boards"
-          chartData={testChartData}
-          colorsInfo={testColors}
-        />
+      <div className={classes.content}>
+        <div className={classes.verticalTabs}>
           <BoardsStatisticsTab
-              title="Your boards"
-              chartData={testChartData}
-              colorsInfo={testColors}
+            title="Your boards"
+            chartData={testChartData}
+            colorsInfo={testColors}
           />
+          <BoardsStatisticsTab
+            title="Your boards"
+            chartData={testChartData}
+            colorsInfo={testColors}
+          />
+        </div>
+        <div className={classes.boardsCardsWrapper}>
+          <Typography className={classes.boardsBlockTitle}>
+            Latest visited boards
+          </Typography>
+          <div className={classes.boardsRow}>
+            <div
+              style={{ backgroundColor: "red" }}
+              className={classes.boardCard}
+            >
+              <Typography
+                className={classes.boardTitle}
+              >
+                TEST 1
+              </Typography>
+            </div>
+            <div
+              style={{ backgroundColor: "yellow" }}
+              className={classes.boardCard}
+            >
+              <Typography className={classes.boardTitle}>TEST 1</Typography>
+            </div>
+            <div
+              style={{ backgroundColor: "gray" }}
+              className={classes.boardCard}
+            >
+              <Typography className={classes.boardTitle}>TEST 1</Typography>
+            </div>
+            <div
+              style={{ backgroundColor: "#2255ff" }}
+              className={classes.boardCard}
+            >
+              <Typography className={classes.boardTitle}>TEST 1</Typography>
+            </div>
+          </div>
+          <Typography className={classes.boardsBlockTitle}>
+            Personal boards
+          </Typography>
+          <div className={classes.boardsRow}>
+            <div className={classes.boardCard}>
+              <Typography className={classes.boardTitle}>TEST 1</Typography>
+            </div>
+            <div className={classes.boardCard}>
+              <Typography className={classes.boardTitle}>TEST 1</Typography>
+            </div>
+            <div className={classes.boardCard}>
+              <Typography className={classes.boardTitle}>TEST 1</Typography>
+            </div>
+            <div className={classes.boardCard}>
+              <Typography className={classes.boardTitle}>TEST 1</Typography>
+            </div>
+            <div className={classes.boardCard}>
+              <Typography className={classes.boardTitle}>TEST 1</Typography>
+            </div>
+            <Button classes={{ root: classes.createNewButton }}>
+              <div className={classes.addNewCard}>
+                <AddCircleOutlineIcon
+                  style={{ fontSize: 50, fill: "#fff" }}
+                ></AddCircleOutlineIcon>
+                <Typography className={classes.createNewButtonText}>
+                  Create new
+                </Typography>
+              </div>
+            </Button>
+          </div>
+          <Typography className={classes.boardsBlockTitle}>
+            Teams boards
+          </Typography>
+          <div className={classes.boardsRow}>
+            <div className={classes.boardCard}>
+              <Typography className={classes.boardTitle}>TEST 1</Typography>
+            </div>
+            <div className={classes.boardCard}>
+              <Typography className={classes.boardTitle}>TEST 1</Typography>
+            </div>
+            <div className={classes.boardCard}>
+              <Typography className={classes.boardTitle}>TEST 1</Typography>
+            </div>
+            <div className={classes.boardCard}>
+              <Typography className={classes.boardTitle}>TEST 1</Typography>
+            </div>
+            <div className={classes.boardCard}>
+              <Typography className={classes.boardTitle}>TEST 1</Typography>
+            </div>
+            <Button classes={{ root: classes.createNewButton }}>
+              <div className={classes.addNewCard}>
+                <AddCircleOutlineIcon
+                  style={{ fontSize: 50, fill: "#fff" }}
+                ></AddCircleOutlineIcon>
+                <Typography className={classes.createNewButtonText}>
+                  Create new
+                </Typography>
+              </div>
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/boards/styles.js
+++ frontend/src/pages/boards/styles.js
@@ -7,11 +7,75 @@ const useStyles = makeStyles({
     width: "100%",
     height: "100%",
   },
+  content: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    height: "100%",
+  },
   verticalTabs: {
     display: "flex",
     flexDirection: "column",
     width: "420px",
-    borderRight: '1px solid #fff'
+    borderRight: "1px solid #fff",
+  },
+  boardsCardsWrapper: {
+    padding: "30px",
+    display: "flex",
+    width: "100%",
+    height: "100%",
+    flexDirection: "column",
+  },
+  addNewCard: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "200px",
+    height: "100px",
+  },
+  createNewButton: {
+    width: "200px",
+    height: "100px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    "&:focus": {
+      outline: "none",
+    },
+  },
+  createNewButtonText: {
+    color: "#fff",
+    fontSize: "15px",
+  },
+  boardsBlockTitle: {
+    fontSize: "18px",
+    fontWeight: "bold",
+    color: "#fff",
+    paddingBottom: "30px",
+  },
+  boardCard: {
+    width: "200px",
+    height: "100px",
+    marginBottom: "30px",
+    backgroundColor: "#fff",
+  },
+  boardsRow: {
+    display: "flex",
+    flexFlow: "wrap",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    width: "100%",
+    flexDirection: "row",
+    marginBottom: "20px",
+  },
+  boardTitle: {
+    paddingLeft: "15px",
+    paddingTop: "10px",
+    fontWeight: "bold",
+    fontSize: "15px",
+    color: "white",
   },
 });
 
