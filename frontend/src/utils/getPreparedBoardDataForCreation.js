f85e87d3086605d5df6487462b29ae158913597a
Yaroslav Nestor <nestorslavko45@gmail.com>
2020-05-20 01:33:19 +0300

    Add invited friends field to mutation

--- frontend/src/utils/getPreparedBoardDataForCreation.js
+++ frontend/src/utils/getPreparedBoardDataForCreation.js
@@ -2,6 +2,7 @@ export const getPreparedBoardDataForCreation = (data) => {
   return {
     name: data.name,
     backgroundColor: data.color,
+    invitedMembers: data.invitedFriends,
     isOpen: data.type === 'Public',
     isPrivate: data.type === 'Private',
   };
